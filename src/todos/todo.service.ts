f4b5b01edbcf96048001f88fe0dc889aedd8175d
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-07-11 16:06:03 +0530

    added todo.service.ts to todos

---
+++ src/todos/todo.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './todo.entity';
+
+@Injectable()
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo) private readonly todoRepository: Repository<Todo>,
+  ) {}
+
+  async create(dto: { title: string }) {
+    const todo = this.todoRepository.create(dto);
+    return await this.todoRepository.save(todo);
+  }
+}
